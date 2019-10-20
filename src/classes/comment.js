86284b6e7091fbae42e57f5b4f2ecb28151fdb79
Puffycheeseses <noahheague@gmail.com>
2019-10-20 14:21:11 +1100

    Fixed validation

--- src/classes/comment.js
+++ src/classes/comment.js
@@ -8,21 +8,28 @@ module.exports.comment = class comment {
   constructor (user, body, parent, db) {
     this.type = `comment`
     this.id = crypto.randomBytes(6).toString('hex')
-    this.user = (db.find(i => i.type==='user'&&i.secret===user.secret?i:false))[0].uid
+    this.user = (db.find(i => i.type==='user'&&i.secret===user.secret?i:false))[0]
     this.body = DOMPurify.sanitize(body)
-    this.parent = parent
+    this.parent = (db.find(i => (i.type==='post'||i.type==='comment')&&i.id===parent?i:false))[0]
     this.date = + new Date()
   }
 
   validate (db) {
-    if(!this.parent) return 'Parent Invalid' // Category doesnt exist
+    if(!this.parent) return 'Parent Invalid' // Parent doesnt exist
     if(!this.user) return 'User Invalid' // User doesnt exists
     if(!this.body) return 'Body Invalid'// does this.body contain anything
+
+    // Set Values
+    this.parent = this.parent.id
+    this.user = this.user.id
+
     return true
   }
 
   async push (db) {
-    if(!this.validate(db)) return {'error': this.validate(db)}
-    return await db.findById(await db.insert(this))
+    const e = this.validate(db);
+    if(typeof e === 'boolean') return await db.findById(await db.insert(this))
+    if(e.indexOf("Invalid") !== -1) return {'error': e}
+    return {'error': 'Big yikes, this should never happen'}
   }
 }
--- src/classes/post.js
+++ src/classes/post.js
@@ -8,7 +8,7 @@ module.exports.post = class post {
   constructor (user, body, category, db) {
     this.type = `post`
     this.id = crypto.randomBytes(6).toString('hex')
-    this.user = (db.find(i => i.type==='user'&&i.secret===user.secret?i:false))[0].uid
+    this.user = (db.find(i => i.type==='user'&&i.secret===user.secret?i:false))[0]
     this.body = DOMPurify.sanitize(body)
     this.category = (db.find(i => i.type==='category'&&i.name===category?i:false))[0]
     this.date = + new Date()
@@ -18,11 +18,17 @@ module.exports.post = class post {
     if(!this.category) return 'Category Invalid' // Category doesnt exist
     if(!this.user) return 'User Invalid' // User doesnt exists
     if(!this.body) return 'Body Invalid'// does this.body contain anything
+
+    // Set Values
+    this.user = this.user.id
+
     return true
   }
 
   async push (db) {
-    if(!this.validate(db)) return {'error': this.validate(db)}
-    return await db.findById(await db.insert(this))
+    const e = this.validate(db);
+    if(typeof e === 'boolean') return await db.findById(await db.insert(this))
+    if(e.indexOf("Invalid") !== -1) return {'error': e}
+    return {'error': 'Big yikes, this should never happen'}
   }
 }
