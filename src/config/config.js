42d9f6639ca9fbeb9acd152764b32d6ba30b4406
Puffycheeseses <noahheague@gmail.com>
2019-10-20 14:40:01 +1100

    Added link to docs

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports.config = {
   port: "8080",
-  docURL: "https://example.com",
+  docURL: "https://github.com/Puffycheeses/Boardlet/wiki",
   dbPath: path.join(__dirname, "db.sl"),
   newUserTimeout: 1
 }
